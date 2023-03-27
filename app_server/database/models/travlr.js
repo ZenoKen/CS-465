48eb9ebf2aa55095e2f0736649624157c6613eea
Kentrell Edwards <kenhundagreat@gmail.com>
2023-03-26 22:03:45 -0500

    Express website, static HTML only

--- app_server/database/models/travlr.js
+++ app_server/database/models/travlr.js
@@ -10,5 +10,8 @@ const tripSchema = new mongoose.Schema({
     perPerson: {type: String, required: true},
     image: {type: String, required: true},
     description: {type: String, required: true}
-});
-mongoose.model('trips', tripSchema);
+}); 
+
+// mongoose.model("trips", tripSchema);
+
+module.exports = mongoose.model("trips", tripSchema);
